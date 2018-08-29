930c76ed10973b16836e5ba2ceaf20e61b5d14d0
myschoollunchapp@gmail.com <myschoollunchapp@gmail.com>
2018-08-29 00:01:36 -0400

    adding user.go

---
+++ routers/user.go
@@ -0,0 +1,13 @@
+package routers
+
+import (
+  "github.com/gorilla/mux"
+  "github.com/myschoollunchapp/server/controllers"
+)
+
+func SetUserRoutes(router *mux.Router) *mux.Router {
+  router.HandleFunc("/users/register", controllers.Register).Methods("POST")
+  router.HandleFunc("/users/login",controllers.Login).Methods("POST")
+  return router
+}
+
